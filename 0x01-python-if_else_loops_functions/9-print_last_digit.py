d05d964bbe04bd8472e24fea69ed36e738f59e8a
khaledareb <97897761+khaledareb@users.noreply.github.com>
2022-09-20 16:59:21 +0300

    9-print

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,1 +1,7 @@
-kjdasdasad:
+#!/usr/bin/python3
+def print_last_digit(number):
+    if number < 0:
+        number = number * -1
+    num = number % 10
+    print("{}".format(num), end='')
+    return 
