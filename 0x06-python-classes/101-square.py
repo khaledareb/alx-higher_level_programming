6ef67f5be13a20ef334b9ae23b014b9683e3e6ce
khaledareb <khaledareb@gmail.com>
2022-10-11 20:46:41 +0300

    Added 101-sqaure

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -1,12 +1,7 @@
 #!/usr/bin/python3
 """Square module.
 
-This module contains a class that defines a square and its size and its
-position on the screen, checking if the given values are right, and a setter
-and getter methods to set or get them. A __str__ method is here to handle the
-use of the builtin print function. There's also an area method that returns
-the area of the square, another one that handles the print of the square.
-
+Refer to REA~DMe.md nd refer too 101- sqaure pu
 """
 
 
